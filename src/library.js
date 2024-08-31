1b1f1907063c94b64d52d5071604af4e90975000
PRASIDDHI <pdm@example.com>
2024-08-31 15:50:15 +0530

    Implement return book feature

--- src/library.js
+++ src/library.js
@@ -19,6 +19,14 @@ class Library {
         book.available = false;
       }
       
+      returnBook(isbn) {
+        const book = this.books.find((b) => b.isbn === isbn && !b.available);
+        if (!book) {
+          throw new Error('Book was not borrowed');
+        }
+        book.available = true;
+      }
+      
   }
   
   module.exports = Library;
